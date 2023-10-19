8d0dbe263870d6b3061ba313a22bc259accb81dd
kossecki-gabrielle <77404045+kossecki-gabrielle@users.noreply.github.com>
2023-10-19 06:28:26 -0700

    Just added argument to match class diagram
    Also put a suggestion for the steps needed to retrieve the nutrient data from the database in the future

--- src/Log.java
+++ src/Log.java
@@ -3,6 +3,7 @@ import java.time.Duration;
 import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class Log {
     Date dateOfEntry;
@@ -25,8 +26,15 @@ class DietLog extends Log {
     public void addIngredient(String ingredient, int quantity){
         this.ingredients.put(ingredient, quantity);
     }
-    public void calculateNutrients(){
+    public void calculateNutrients(<List<List<String>> nutrientAmount){
         //Use nutrient amount, nutrient name, food group?, food name? from database to get the nutrient amounts
+        /* Send FOOD NAME.FoodDescription (complete food name in english, equivalent to ingredient) to database
+    	 * Get FOOD NAME.FOODID from FOOD NAME csv file
+    	 * Send FOODID to NUTRIENT AMOUNT to obtain NutrientNameID and its corresponding NutrientValue
+    	 * Send NutrientNameID to NUTRIENT NAME file to get corresponding NutrientName
+    	 * Return (NutrientName, NutrientValue)
+    	 * Maybe? Can revise later
+    	 */
     }
 
     public void addNutrientValue(String nutrient, double amount){
