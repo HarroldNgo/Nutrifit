68cf9714ca9cc55f266ccba98a20d712ebd280f8
kossecki-gabrielle <77404045+kossecki-gabrielle@users.noreply.github.com>
2023-10-19 05:44:21 -0700

    Update UserProfile.java
    Edited some variable types
    Added units as argument of UserProfile
    Added chooseUserProfile() to match sequence diagram

--- src/UserProfile.java
+++ src/UserProfile.java
@@ -1,27 +1,28 @@
 import java.util.Date;
 public class UserProfile {
     private String name;
-    private String sex;
+    private char sex;
     private Date dateOfBirth;
     private double height;
     private double weight;
 
     //Maybe we can use boolean, 0 = metric, 1 = imperial?
-    private String units = "metric";
+    private Boolean units = "metric";
 
-    public UserProfile(String name, String sex, Date date, double height, double weight){
+    public UserProfile(String name, char sex, Date date, double height, double weight, Boolean units){
         this.name = name;
         this.sex=sex;
         this.dateOfBirth = date;
         this.height = height;
         this.weight = weight;
+        this.units = units; //user should be able to select/indicate unit type in profile creation
 
         //add to database
     }
     public void deleteUserProfile(){
 
     }
-    public void editUserProfile(UserProfile profile, String newName, String newSex, Date newDate, double newHeight, double newWeight, String newUnits){
+    public void editUserProfile(UserProfile profile, String newName, char newSex, Date newDate, double newHeight, double newWeight, String newUnits){
         profile.name = newName;
         profile.sex = newSex;
         profile.dateOfBirth = newDate;
@@ -30,6 +31,10 @@ public class UserProfile {
         profile.units = newUnits;
 
         //update to database
+    }
+    public void chooseUserProfile(UserProfile profile) {
+        //request userProfile data from database
+        //return returned userProfile data to be displayed by ProfileGUI
     }
     public String getName(){
         return this.name;
